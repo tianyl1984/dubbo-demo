6f2dc71d1a41c65abf0b6aa8395af8feffa48671
tianyale <tianyale@ufenqi.com>
2017-03-08 14:17:14 +0800

    demo

--- dubbo-consumer/src/main/java/com/tianyl/dubboDemo/consumer/ConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/tianyl/dubboDemo/consumer/ConsumerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.tianyl.dubboDemo.api.ICallbackDemoService;
+import com.tianyl.dubboDemo.api.ITimeService;
 
 @SpringBootApplication
 public class ConsumerApplication {
@@ -12,12 +12,12 @@ public class ConsumerApplication {
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(ConsumerApplication.class, args);
 		System.out.println("start");
-		// ITimeService timeService = context.getBean(ITimeService.class);
-		// System.out.println("获取服务器时间：" + timeService.time());
+		ITimeService timeService = context.getBean(ITimeService.class);
+		System.out.println("获取服务器时间：" + timeService.time());
 		// ICallTimeService timeService = context.getBean(ICallTimeService.class);
 		// System.out.println("获取服务器时间：" + timeService.callTime());
-		ICallbackDemoService callbackService = context.getBean(ICallbackDemoService.class);
-		callbackService.call("张三", new CallbackListenerImpl());
+		// ICallbackDemoService callbackService = context.getBean(ICallbackDemoService.class);
+		// callbackService.call("张三", new CallbackListenerImpl());
 		System.in.read();
 	}
 
